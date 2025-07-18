a30d3c839faefe139baeb7278afe243cec898fc3
Japson <17816853746@163.com>
2025-07-18 15:12:44 +0800

    日志调整

--- custom-spring-scaffolds/newland-product-scaffold-ctx/newland-spring-scaffold-ctx/src/main/java/com/nlecloud/spring/scaffold/NewLandSpringConfig.java
+++ custom-spring-scaffolds/newland-product-scaffold-ctx/newland-spring-scaffold-ctx/src/main/java/com/nlecloud/spring/scaffold/NewLandSpringConfig.java
@@ -5,23 +5,18 @@ import com.baomidou.mybatisplus.extension.plugins.handler.TenantLineHandler;
 import com.nlecloud.spring.scaffold.api.user.IUPMSUserApi;
 import com.nlecloud.spring.scaffold.common.UserProxy;
 import com.nlecloud.spring.scaffold.debug.DebugConfig;
-import com.nlecloud.spring.scaffold.filter.PermissionInterceptor;
 import com.nlecloud.spring.scaffold.filter.UserFeignInterceptor;
 import com.nlecloud.spring.scaffold.filter.UserInterceptor;
 import com.nlecloud.spring.scaffold.handle.*;
 import com.nlecloud.spring.scaffold.service.DictServiceProxy;
-import feign.Feign;
 import net.github.fastdev.boot.CustomSpringBootConfig;
 import net.github.fastdev.boot.handle.ComEnumDisplayHandle;
 import org.bouncycastle.util.Arrays;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.AutoConfigureBefore;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.cloud.openfeign.EnableFeignClients;
-import org.springframework.cloud.openfeign.FeignClientFactoryBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
@@ -125,10 +120,10 @@ public class NewLandSpringConfig {
         return new TraceServiceHandle();
     }
 
-    @Bean
-    public PermissionInterceptor permissionInterceptor(){
-        return new PermissionInterceptor();
-    }
+//    @Bean
+//    public PermissionInterceptor permissionInterceptor(){
+//        return new PermissionInterceptor();
+//    }
 
     @Bean
     @ConditionalOnProperty(prefix = "nlecloud.product",name = "tenantEnabled",havingValue = "true")
