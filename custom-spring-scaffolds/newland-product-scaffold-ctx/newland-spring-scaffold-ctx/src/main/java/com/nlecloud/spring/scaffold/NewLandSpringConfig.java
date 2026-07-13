49700d5a6f2827eff07278ddc4b550956b476d65
Japson <17816853746@163.com>
2026-07-13 11:52:48 +0800

    增加用户缓存调整为protocol协议

--- custom-spring-scaffolds/newland-product-scaffold-ctx/newland-spring-scaffold-ctx/src/main/java/com/nlecloud/spring/scaffold/NewLandSpringConfig.java
+++ custom-spring-scaffolds/newland-product-scaffold-ctx/newland-spring-scaffold-ctx/src/main/java/com/nlecloud/spring/scaffold/NewLandSpringConfig.java
@@ -18,6 +18,7 @@ import net.github.fastdev.boot.handle.ComEnumDisplayHandle;
 import org.apache.dubbo.common.logger.FluentLogger;
 import org.bouncycastle.util.Arrays;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.AutoConfigureBefore;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
@@ -28,7 +29,9 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 import org.springframework.context.annotation.Lazy;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.RedisSerializer;
 
 import java.util.HashSet;
 import java.util.Optional;
@@ -69,8 +72,21 @@ public class NewLandSpringConfig {
      *@author Japson Huang
      *
     */
+    @Bean("userCacheRedisTemplate")
+    public RedisTemplate<String, byte[]> userCacheRedisTemplate(RedisConnectionFactory connectionFactory) {
+        RedisTemplate<String, byte[]> template = new RedisTemplate<>();
+        template.setConnectionFactory(connectionFactory);
+        template.setKeySerializer(RedisSerializer.string());
+        template.setValueSerializer(RedisSerializer.byteArray());
+        template.setHashKeySerializer(RedisSerializer.string());
+        template.setHashValueSerializer(RedisSerializer.byteArray());
+        return template;
+    }
+
     @Bean
-    public UserProxy userProxy(@Lazy IUPMSUserApi iupmsUserApi, @Autowired(required = false)UserInfoService userInfoService, RedisTemplate<String,String> redisTemplate){
+    public UserProxy userProxy(@Lazy IUPMSUserApi iupmsUserApi,
+                               @Autowired(required = false) UserInfoService userInfoService,
+                               @Qualifier("userCacheRedisTemplate") RedisTemplate<String, byte[]> redisTemplate) {
         UserInfoService finalService = userInfoService != null
                 ? userInfoService
                 : (id) -> iupmsUserApi.getUserDetailById(id);
