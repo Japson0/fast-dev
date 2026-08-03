129ad220c30b4155d80527c5e5adf6f42a17a7ba
Japson <17816853746@163.com>
2026-08-03 10:53:10 +0800

    增加租户角色

--- custom-spring-scaffolds/newland-product-scaffold-ctx/newland-spring-scaffold-ctx/src/main/java/com/nlecloud/spring/scaffold/api/user/IUPMSUserApi.java
+++ custom-spring-scaffolds/newland-product-scaffold-ctx/newland-spring-scaffold-ctx/src/main/java/com/nlecloud/spring/scaffold/api/user/IUPMSUserApi.java
@@ -5,7 +5,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "nlecloud-upms-user-center",contextId = "getUserId",url = "http://nlecloud-upms-user-center-server.upms:19192")
+@FeignClient(name = "nlecloud-upms-user-center",contextId = "getUserId",url = "${upms-user-center.url:http://nlecloud-upms-user-center-server.upms:19192}")
 public interface IUPMSUserApi {
 
     @GetMapping("/api/user/detail/{id}")
